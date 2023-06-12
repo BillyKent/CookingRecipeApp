a722b4ffdd6196419431a4fb6dca0f07bf9b2d5e
Billy Kent <billykentac@gmail.com>
2023-06-12 11:22:53 -0500

    Update of BASE_URL

--- data/src/main/java/com/billysoft/data/util/DataModuleConstants.kt
+++ data/src/main/java/com/billysoft/data/util/DataModuleConstants.kt
@@ -3,7 +3,7 @@ package com.billysoft.data.util
 internal object DataModuleConstants {
 
     // Network
-    const val BASE_URL = "http://billysoft.mockable.io/"
+    const val BASE_URL = "http://demo1142242.mockable.io/"
     const val CONNECT_TIMEOUT_IN_SECONDS = 120L
 
 }
